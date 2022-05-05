e0af38378e377d90e43854098c5f0ad82f10b6e1
Ganesh12345678o <100017495+Ganesh12345678o@users.noreply.github.com>
2022-05-05 12:15:19 +0530

    import java.util.Scanner; class java {
            public static void main(String args[])
            {
        Scanner sc=new Scanner(System.in);
        float a,b,c,s;
        float area;
        a=sc.nextFloat();
        b=sc.nextFloat();
        c=sc.nextFloat();
        s=(a+b+c)/2;
        area=(float)Math.sqrt(s*(s-a)*(s-b)*(s-c));
        System.out.printf("%.2f",area);
            }
        
    }

--- Area_of_the_Triangle.java
+++ Area_of_the_Triangle.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
-class Area
+class java
 {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        float a,b,c,s;
-        float area;
-        a=sc.nextFloat();
-        b=sc.nextFloat();
-        c=sc.nextFloat();
-        s=(a+b+c)/2;
-        area=(float)Math.sqrt(s*(s-a)*(s-b)*(s-c));
-        System.out.printf("%.2f",area);
-    }
+    
+        public static void main(String args[])
+        {
+    Scanner sc=new Scanner(System.in);
+    float a,b,c,s;
+    float area;
+    a=sc.nextFloat();
+    b=sc.nextFloat();
+    c=sc.nextFloat();
+    s=(a+b+c)/2;
+    area=(float)Math.sqrt(s*(s-a)*(s-b)*(s-c));
+    System.out.printf("%.2f",area);
+        }
+    
 }
